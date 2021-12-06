4dc92aa33d2812cc0efd13d4eb47171b38bba801
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-12-06 18:19:40 -0300

    Ruby: day1 part2

--- day1/main.rb
+++ day1/main.rb
@@ -1,7 +1,21 @@
 module Day1
-  def self.solution(file_path)
+  def self.solution1(file_path)
     file = File.read(file_path)
     depths = file.split("\n").map(&:to_i)
+    return depth_count(depths)
+  end
+
+  def self.solution2(file_path)
+    file = File.read(file_path)
+    depths = file.split("\n").map(&:to_i)
+    grouped_depths = []
+    (0..(depths.length - 3)).to_a.each do |depth_index|
+      grouped_depths << depths[depth_index..depth_index + 2].sum
+    end
+    return depth_count(grouped_depths)
+  end
+
+  def self.depth_count(depths)
     count = 0
     old_depth = depths[0]
     depths[1..-1].each do |depth|
@@ -12,4 +26,5 @@ module Day1
   end
 end
 
-puts Day1.solution('input.txt')
+puts 'Solution 1: ', Day1.solution1('input.txt')
+puts 'Solution 2: ', Day1.solution2('input.txt')
--- day1/main_spec.rb
+++ day1/main_spec.rb
@@ -1,9 +1,19 @@
 require './main.rb'
 
-describe 'Day1#solution' do
-  context "when have example input" do
-    it "returns 7" do
-      expect(Day1.solution('example_input.txt')).to eq(7)
+describe Day1 do
+  describe '#solution1' do
+    context "when have example input" do
+      it "returns 7" do
+        expect(Day1.solution1('example_input.txt')).to eq(7)
+      end
+    end
+  end
+
+  describe '#solution2' do
+    context "when have example input" do
+      it "returns 5" do
+        expect(Day1.solution2('example_input.txt')).to eq(5)
+      end
     end
   end
 end
